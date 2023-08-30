74e434d6962afc3e0a7d37d9347241b04c17a4d6
Ethan Niblock <ethan.niblock@exitex.ie>
2023-08-30 16:34:53 +0100

    BlackJack C# files added

---
+++ Blackjack/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using System.Reflection.Metadata.Ecma335;
+using System.Runtime.InteropServices;
+
+namespace BlackjackTest 
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            
+            int menuChoice = 1;
+
+            while(menuChoice != 0)
+            {
+                string menu = @"**********************************
+*              Menu              *
+*         Press 1 to Play        *
+*         Press 0 to Exit        *
+*                                *
+**********************************";
+                Console.WriteLine(menu);
+                menuChoice = int.Parse(Console.ReadLine());
+
+                int gameOver = 1;
+                var randNum = new Random();
+                while(gameOver != 0)
+                {
+                    while(totalNumDeal < 21 || totalNumPlay < 21 || hit != 0)
+                    {
+
+                    }
+                }
+            }
+
+
+        }
+    }
+}
